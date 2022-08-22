90840a01c98a485822559dac15e6f16e65d48745
Arjay C. Balboa <111472877+Chopyzx@users.noreply.github.com>
2022-08-22 11:07:58 +0800

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,23 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    int archer;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    public void sayMystrength() {
+        System.out.println("My strength " + strength);
+    }
+    public void sayMyagility() {
+        System.out.println("My agility " + agility);
+    }
+    public void sayMyintelligence() {
+        System.out.println("My intelligence " + intelligence);
+    }
+    public void sayMyarcher() {
+        System.out.println("My archer " + archer);
+    }
+} 
